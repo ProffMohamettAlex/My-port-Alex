de94ef1d47ef3a63d56cf48b2b94c46da819bd2e
AbstractProgrammer <AbstractProgrammer@example.com>
2023-07-28 00:25:56 +0300

    alex

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
   
   return (
     <footer>
-      <a href="#home" className="footer__logo">Proff Mohamett Tahliil <b>ALex</b></a>
+      <a href="#home" className="footer__logo">Proff Mohamett Tahliil</a>
       <ul className="permalinks">
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
